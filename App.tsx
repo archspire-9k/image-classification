e2c177e0a9382324a10b16188cca998a9ee327ee
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-05-27 10:26:47 -0400

    clean lifecycle

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { View, Text, Image, Button } from 'react-native';
 import * as tf from '@tensorflow/tfjs';
 import { fetch, decodeJpeg } from '@tensorflow/tfjs-react-native';
 import * as mobilenet from '@tensorflow-models/mobilenet';
+import {MobileNet} from "@tensorflow-models/mobilenet"
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
 
@@ -10,7 +11,9 @@ const App = () => {
   const [isTfReady, setIsTfReady] = useState(false);
   const [result, setResult] = useState('');
   const [imagePick, setImagePick] = useState("");
-  const [error, setError] = useState();
+  const [error, setError] = useState("");
+  const [model, setModel] = useState({} as MobileNet);
+  const [ready, setReady] = useState(true);
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -19,30 +22,50 @@ const App = () => {
       quality: 1,
     });
 
+    setResult("");
+    setReady(false);
     if (!resultImage.canceled) {
       setImagePick(resultImage.assets[0].uri);
+      setError("");
       // console.log("canceled")
-      load();
+      await process(resultImage.assets[0].uri);
     }
+    
   };
 
   const load = async () => {
     try {
       // Load mobilenet.
       await tf.ready();
-      const model = await mobilenet.load().catch(error => console.log(error, "test"));
-      setIsTfReady(true);
+      await mobilenet.load().catch(error => console.log(error, "test"))
+      .then(model => {
+        if(model) {
+          setModel(model);
+          setIsTfReady(true);
+        }}); 
+    }
+    catch (err) {
+      if (err) {
+        setError(err.toString() + " error loading model");
+      }
+    }
+  }
 
-      // Start inference and show result.
-      // const image = require('./basketball.jpg');
-      // const imageAssetPath = Image.resolveAssetSource(image);
-      // const response = await fetch(imageAssetPath.uri, {}, { isBinary: true });
-      const response = await FileSystem.readAsStringAsync(imagePick, { encoding: FileSystem.EncodingType.Base64 });
+  const process = async (uri : string) => {
+    try {
+      // Start inference and show result.      
+
+      const response = await FileSystem.readAsStringAsync(uri, { encoding: FileSystem.EncodingType.Base64 }).catch(err =>{
+        if (err) {
+          setError(err.toString() + " reading file prob");
+        }
+      } );
       // const imageDataArrayBuffer = await response.arrayBuffer();
       const buffer = Buffer.from(response, 'base64');
       const imageData = new Uint8Array(buffer);
       
       const imageTensor = decodeJpeg(imageData);
+      
       if (model) {
         const prediction = await model.classify(imageTensor);
         if (prediction && prediction.length > 0) {
@@ -51,10 +74,17 @@ const App = () => {
           );
         }
       }
+      setReady(true);
     } catch (err) {
-      setError(err)
+      if (err) {
+        setError(err.toString() + " processing problem");
+      }
     }
-  };
+  }
+
+  useEffect(() => {
+    load();
+  }, [])
 
   return (
     <View
@@ -66,12 +96,12 @@ const App = () => {
         justifyContent: 'center',
       }}
     >
-      <Button title="Pick an image from camera roll" disabled={!isTfReady} onPress={pickImage} />
+      <Button title="Pick an image from camera roll" disabled={!isTfReady || !ready} onPress={pickImage} />
       {imagePick && <Image source={{ uri: imagePick }} style={{ width: 200, height: 200 }} />}
       {!isTfReady && <Text>Loading TFJS model...</Text>}
       {isTfReady && result === '' && <Text>Classifying...</Text>}
-      {result !== '' && <Text>{result}</Text>}
-      {error && <Text>{error}</Text>}
+      {ready && <Text>{result}</Text>}
+      {error != "" && <Text>{error}</Text>}
     </View>
   );
 };
